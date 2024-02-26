7f7a82668e373952dfdb4c7ad3d701a30920aa79
Ayush Kumar <ayushishan08@gmail.com>
2024-02-27 00:52:29 +0530

    done some CSES problems

---
+++ src/BinarySearchSQRT.java
@@ -0,0 +1,34 @@
+public class BinarySearchSQRT {
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3; // precision till 3 decimal digits
+        System.out.printf("%.3f", sqrt(n, p));
+    }
+    //TC ---> O(log(n))
+    static double sqrt(int n, int p) {
+        int start = 0;
+        int end = n;
+
+        double root = 0.0;
+        while (start <= end) {
+            int m = start + (start - end)/2;
+            if(m * m == n) {
+                return m;
+            }
+            if(m * m > n) {
+                end = m - 1;
+            } else {
+                start = m + 1;
+            }
+        }
+        double increment = 0.1;
+        for(int i = 0; i < p;i++) {
+            while (root * root <= n) {
+                root += increment;
+            }
+            root -= increment;
+            increment /= 10;
+        }
+        return root;
+    }
+}
