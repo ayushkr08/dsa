ceae51244773f2f8c5e7ed49453c82477e4887f6
Ayush Kumar <ayushishan08@gmail.com>
2024-03-01 16:11:53 +0530

    some questions on linkedlist

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        linked list = new linked();
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(23);
+        list.insert(100,3);
+
+        list.display();
+
 
     }
 
--- src/subString.java
+++ src/subString.java
@@ -4,7 +4,7 @@ public class subString {
 
         Integer num2 = num1;
         Integer num3 = num1;
-
+        Integer num4 = 12;
         System.out.println(num1 == num2);
         System.out.println(num2 == num3);
 
