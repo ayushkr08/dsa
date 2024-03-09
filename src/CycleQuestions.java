1d4bd088fcc8b158cd49fbe2c3cf3e79891d9b51
Ayush Kumar <ayushishan08@gmail.com>
2024-03-09 20:36:44 +0530

    merge sort in linked list

--- src/CycleQuestions.java
+++ src/CycleQuestions.java
@@ -91,6 +91,17 @@ public class CycleQuestions {
         return ans;
     }
 
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+
 }
 class ListNode {
     int val;
---
+++ src/MergeSort.java
@@ -0,0 +1,43 @@
+public class MergeSort {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null){
+            if(list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+//        if(list1 != null) {
+//            tail.next = list1;
+//        } else {
+//            list1 = list2;
+//        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+    ListNode getMid(ListNode head) {
+        ListNode midprev = null;
+        while (head != null && head.next != null) {
+            midprev = (midprev == null) ? head : midprev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midprev.next;
+        midprev.next = null;
+        return mid;
+    }
+}
