a4a7197c19ffe298f9326a46297a122f9314a279
Ayush Kumar <ayushishan08@gmail.com>
2024-03-12 21:08:48 +0530

    leetcode questions

--- src/linked.java
+++ src/linked.java
@@ -274,6 +274,30 @@ public class linked {
         return false;
     }
 
+    public void reorderList(ListNode head) {
+        if(head == null && head.next == null) {
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
+
+        //rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+        //next of tail to null
+        if(hf != null) {
+            hf.next = null;
+        }
+    }
+
 
     public static void main(String[] args) {
         linked first = new linked();
