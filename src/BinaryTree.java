50cef411711b4edab5de8397b5baa4f8fd4742ff
Ayush Kumar <ayushishan08@gmail.com>
2024-03-26 12:44:59 +0530

    binary tree populate and display functions

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -40,4 +40,48 @@ public class BinaryTree {
             populate(scanner, node.right);
         }
     }
+
+    public void display() {
+        display(this.root, "");
+    }
+
+    private void display(Node node, String indent) {
+        if(node == null) {
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+    }
+
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
+    private void prettyDisplay(Node node, int level) {
+        if(node == null) {
+            return;
+        }
+        prettyDisplay(node.right, level + 1);
+
+        if(level != 0) {
+            for(int i = 0; i < level - 1; i++) {
+                System.out.println("|\t\t");
+            }
+            System.out.println("--------->" + node.value);
+        } else {
+            System.out.println(node.value);
+        }
+
+        prettyDisplay(node.left, level + 1);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.prettyDisplay();
+    }
+
 }
