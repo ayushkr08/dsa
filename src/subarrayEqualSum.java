82422564cd75624ac0a70373f41738c700c34b8f
Ayush Kumar <ayushishan08@gmail.com>
2024-04-05 13:29:48 +0530

    subarray find sum

--- src/subarrayEqualSum.java
+++ src/subarrayEqualSum.java
@@ -7,8 +7,9 @@ public class subarrayEqualSum {
         HashSet<Integer> set = new HashSet<>();
         for(int i = 0; i < n - 1; i++) {
             int sum = nums[i] + nums[i + 1];
-            set.add(sum);
-
+            if(!set.add(sum)) {
+                return true;
+            }
             for(int num : set) {
                 System.out.println(num + " ");
             }
@@ -19,7 +20,6 @@ public class subarrayEqualSum {
     public static void main(String[] args) {
         int[] nums = {4, 2, 4};
 //        int[] nums = {1, 2, 3, 4, 5};
-
         findSubarrays(nums);
     }
 }
